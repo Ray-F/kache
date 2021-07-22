eb3b21bb068b3663939352681b8348697687dad6
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-23 01:30:38 +1200

    Change to object destructuring for env variables

--- server/src/util/Config.ts
+++ server/src/util/Config.ts
@@ -2,10 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI;
-const ETH_NODE_URL = process.env.ETH_NODE_URL;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const DEFAULT_WALLET_ADDRESS = "0xc4B7faE9ea8bEc3F1966Db6c89F404173D5398ea"
+const DEFAULT_WALLET_ADDRESS = '0xc4B7faE9ea8bEc3F1966Db6c89F404173D5398ea';
+
+const {
+  MONGODB_URI,
+  ETH_NODE_URL,
+  ETHERSCAN_API_KEY,
+} = process.env;
 
 /*
  * The default port of the application
