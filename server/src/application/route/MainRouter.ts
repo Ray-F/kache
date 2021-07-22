7175e1b409919de9b52119522b57fd8048269455
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-23 01:29:21 +1200

    Remove WalletController as it is no longer relevant for testing

--- server/src/application/route/MainRouter.ts
+++ server/src/application/route/MainRouter.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { DefaultController } from '../controller/DefaultController';
-import { WalletController } from '../controller/WalletController';
 
 /*
  * Main routing file to manage all application route.
@@ -10,12 +9,6 @@ const router = Router();
 
 const defaultController = new DefaultController();
 
-// Wallet
-const walletController = new WalletController();
-const walletRouter = Router();
-router.use('/api', walletRouter);
-walletRouter.use('/transactions', walletController.transactionsReceived);
-walletRouter.use('/balance', walletController.balance);
 
 router.use('/api', defaultController.api404);
 
