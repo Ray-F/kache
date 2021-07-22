91ce26bf0ab1ce7ec98507da3a4d076d3ad40436
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-23 01:31:18 +1200

    Add COINLAYER_ACCESS_KEY to env

--- server/src/util/Config.ts
+++ server/src/util/Config.ts
@@ -8,6 +8,7 @@ const {
   MONGODB_URI,
   ETH_NODE_URL,
   ETHERSCAN_API_KEY,
+  COINLAYER_ACCESS_KEY
 } = process.env;
 
 /*
@@ -16,6 +17,7 @@ const {
 const PORT = process.env.PORT || 9002;
 
 export default {
+  COINLAYER_ACCESS_KEY,
   DEFAULT_WALLET_ADDRESS,
   ETHERSCAN_API_KEY,
   ETH_NODE_URL,
