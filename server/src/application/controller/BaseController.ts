e975fdaf6e9395c1eb76da5ca1ddcd9921081898
Raymond Feng <rf.raymondfeng@gmail.com>
2021-07-23 01:29:37 +1200

    Update MongoAdapter DB build with dev database

--- server/src/application/controller/BaseController.ts
+++ server/src/application/controller/BaseController.ts
@@ -2,7 +2,7 @@ import { MongoAdapter } from '../../infrastructure/MongoAdapter';
 import Config from '../../util/Config';
 
 // TODO: Replace this db-name with the correct name
-MongoAdapter.build(Config.MONGODB_URI, '<db-name>');
+MongoAdapter.build(Config.MONGODB_URI, 'kache-dev');
 
 /**
  * The root controller that all other controllers will extend from.
