9bab71310914e3158d27150cf73703764b4a53d2
Michael Howell <michael@nunc.co.nz>
2021-07-27 13:41:38 +1200

    Add themeing template

---
+++ kache-frontend/src/styles/theme.js
@@ -0,0 +1,25 @@
+
+// Document to store informatio in regards to the theme of the frontend
+
+const theme = {
+  coloursHex: {
+    primaryLight: '',
+    primaryDark: '',
+    secondaryLight: '',
+    secondaryDark: ''
+  },
+  
+  coloursRGB: {
+    primaryLight: '',
+    primaryDark: '',
+    secondaryLight: '',
+    secondaryDark: ''
+  },
+  
+  fonts: {
+    primary: '',
+    secondary: ''
+  }
+}
+
+export default theme;
